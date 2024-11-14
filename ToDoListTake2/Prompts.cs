2b07b720969740325725a56ee4fa55d75f1d4f78
Carly Mead <meadcarly16@gmail.com>
2024-11-14 09:14:47 -0700

    Added to line 11

--- ToDoListTake2/Prompts.cs
+++ ToDoListTake2/Prompts.cs
@@ -8,7 +8,7 @@ public class Prompts
         Console.WriteLine();
         Console.WriteLine("1. View your ToDo List");
         Console.WriteLine("2. Add a new task to your ToDo List");
-        Console.WriteLine("3. Update a task: add a day you plan to tackle the task!");
+        Console.WriteLine("3. Update a task: add a day that you plan to tackle the task!");
         Console.WriteLine("4. Update a task status on your ToDo List");
         Console.WriteLine("5. Delete a task from your ToDo List");
         Console.WriteLine("6. Exit");
