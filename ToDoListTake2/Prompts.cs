32091567b1219e9bca7abc8e72d992b1ded359b9
FuChai Mead <fmead@q4justice.com>
2024-11-14 09:49:34 -0700

    update prompt

--- ToDoListTake2/Prompts.cs
+++ ToDoListTake2/Prompts.cs
@@ -4,13 +4,13 @@ public class Prompts
 {
     public static void WelcomeMenu()
     {
-        Console.WriteLine("Welcome to ToDo, where we help you organize your priorities! Tell me where you would like to start...");
+        Console.WriteLine("Welcome to ToDo, where we help you organize your priorities! Where would you like to start?");
         Console.WriteLine();
         Console.WriteLine("1. View your ToDo List");
         Console.WriteLine("2. Add a new task to your ToDo List");
         Console.WriteLine("3. Update a task: add a day that you plan to tackle the task! Hopefully it's a fun one!");
         Console.WriteLine("4. Update a task status on your ToDo List");
-        Console.WriteLine("5. Delete a task from your ToDo List");
+        Console.WriteLine("5. Delete a task from your ToDo List. Remove task.");
         Console.WriteLine("6. Exit");
     }
 }
